4d3dd9bbca703ce02d7e221d06e8e882abade533
Aron Strandberg <strandberg.aron@gmail.com>
2015-12-23 19:23:09 +0100

    Fix long lines

--- 6.rb
+++ 6.rb
@@ -4,8 +4,8 @@
 # The square of the sum of the first ten natural numbers is,
 # (1 + 2 + ... + 10)2 = 552 = 3025
 
-# Hence the difference between the sum of the squares of the first ten natural numbers and the square of the sum is 3025 − 385 = 2640.
-
+# Hence the difference between the sum of the squares of the first ten natural numbers
+# and the square of the sum is 3025 − 385 = 2640.
 # Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
 
 sum_squares = (1..100).to_a.map {|n| n * n}.inject(:+)
