a707cea20437f3a041caf958537a44591cf4d93e
Aron Strandberg <strandberg.aron@gmail.com>
2016-01-19 15:28:59 +0100

    problem 45

--- 45.rb
+++ 45.rb
@@ -1,5 +1,5 @@
-triangles = (285..10000).map { |n| n*(n+1)/2 }
-pentagons = (1..10000).map { |n| n*(3*n+1)/2 }
-hexagons = (1..10000).map { |n| n*(2*n-1) }
+triangles = (286..100000).map { |n| n*(n+1)/2 }
+pentagons = (1..100000).map { |n| n*(3*n-1)/2 }
+hexagons = (1..100000).map { |n| n*(2*n-1) }
 
 puts triangles.find { |n| pentagons.include?(n) && hexagons.include?(n) }
