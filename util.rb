494615c8be5e8127b7c0a2a73e8c925a79dd4778
Aron Strandberg <strandberg.aron@gmail.com>
2016-03-16 15:59:48 +0100

    additions to utilities

--- util.rb
+++ util.rb
@@ -2,6 +2,10 @@ require 'prime'
 
 MILLION = 1_000_000
 
+def primes_upto(n)
+  Prime.each(n).to_a
+end
+
 class Integer
   def digits
     return self.to_s.chars.to_a.map(&:to_i)
@@ -18,11 +22,28 @@ class Integer
   def permutation
     self.digits.permutation.map(&:join).map(&:to_i).uniq
   end
+
+  def prime_factors
+    n = self
+    wheel = [1,2,2,4,2,4,2,4,6,2,6]
+    w, f, fs = 0, 2, []
+    while f*f <= n
+      while n % f == 0
+        fs.push f
+        n = n/f
+      end
+      f += wheel[w]
+      w += 1
+      w = 3 if w == 11
+    end
+    fs.push n if n > 1
+    fs
+  end
 end
 
 class Array
   def sum
-    self.reduce(:+)
+    self.reduce(0, :+)
   end
 
   def to_integers
